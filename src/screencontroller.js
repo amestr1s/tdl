e1e78f315a36b158f1efec5520e6f132ebc3041b
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-10-19 23:21:56 +0300

    Create screencontroller

---
+++ src/screencontroller.js
@@ -0,0 +1,3 @@
+import { addTodoToProject, deleteTodoInProj, changeTodoPrioInProj, changeTodoStatusInProj } from "./coordinator";
+import { addProject, deleteProject, getProjects } from "./projectManager";
+
